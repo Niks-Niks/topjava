09571337ef1b9990c82b731a1449dbe71f719c12
Nikita Pozniakov <wingwen.np@gmail.com>
2020-10-19 22:09:06 +0300

    Hw-3. Realization HW-3.

---
+++ src/test/java/ru/javawebinar/topjava/MealTest.java
@@ -0,0 +1,57 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealTest {
+    public static final int MEAL_USER_ID = START_SEQ + 2;
+    public static final int MEAL_ADMIN_ID = START_SEQ + 9;
+    public static final int NOT_FOUND_MEAL_ID = 11;
+
+    public static final Meal userMeal1 = new Meal(MEAL_USER_ID, LocalDateTime.of(2020, 1, 30, 10, 0, 0), "Завтрак", 500);
+    public static final Meal userMeal2 = new Meal(MEAL_USER_ID + 1, LocalDateTime.of(2020, 1, 30, 13, 0, 0), "Обед", 1000);
+    public static final Meal userMeal3 = new Meal(MEAL_USER_ID + 2, LocalDateTime.of(2020, 1, 30, 20, 0, 0), "Ужин", 500);
+    public static final Meal userMeal4 = new Meal(MEAL_USER_ID + 3, LocalDateTime.of(2020, 1, 31, 0, 0, 0), "Еда на граничное значение", 100);
+    public static final Meal userMeal5 = new Meal(MEAL_USER_ID + 4, LocalDateTime.of(2020, 1, 31, 10, 0, 0), "Завтрак", 1000);
+    public static final Meal userMeal6 = new Meal(MEAL_USER_ID + 5, LocalDateTime.of(2020, 1, 31, 13, 0, 0), "Обед", 500);
+    public static final Meal userMeal7 = new Meal(MEAL_USER_ID + 6, LocalDateTime.of(2020, 1, 31, 20, 0, 0), "Ужин", 410);
+    public static final Meal adminMeal1 = new Meal(MEAL_ADMIN_ID, LocalDateTime.of(2020, 1, 30, 9, 0, 0), "Завтрак админ", 500);
+    public static final Meal adminMeal2 = new Meal(MEAL_ADMIN_ID + 1, LocalDateTime.of(2020, 1, 30, 14, 0, 0), "Обед админ", 1000);
+    public static final Meal adminMeal3 = new Meal(MEAL_ADMIN_ID + 2, LocalDateTime.of(2020, 1, 30, 19, 0, 0), "Ужин админ", 500);
+    public static final Meal adminMeal4 = new Meal(MEAL_ADMIN_ID + 3, LocalDateTime.of(2020, 1, 31, 9, 30, 0), "Завтрак админ", 800);
+    public static final Meal adminMeal5 = new Meal(MEAL_ADMIN_ID + 4, LocalDateTime.of(2020, 1, 31, 14, 10, 0), "Обед админ", 700);
+    public static final Meal adminMeal6 = new Meal(MEAL_ADMIN_ID + 5, LocalDateTime.of(2020, 1, 31, 20, 0, 0), "Ужин админ", 600);
+
+    public static List<Meal> userMeals = Arrays.asList(userMeal7, userMeal6, userMeal5, userMeal4, userMeal3, userMeal2, userMeal1);
+    public static List<Meal> adminMealsFrom30th = Arrays.asList(adminMeal3, adminMeal2, adminMeal1);
+    public static List<Meal> adminMeals = Arrays.asList(adminMeal6, adminMeal5, adminMeal4, adminMeal3, adminMeal2, adminMeal1);
+
+    public static final Meal meal = new Meal(LocalDateTime.of(2020, 9, 1, 10, 0, 0), "Завтрак create", 430);
+
+    public static Meal getUpdated(int mealId) {
+        Meal updated = new Meal(meal);
+        updated.setId(mealId);
+        updated.setDescription("Обновлённая еда");
+        return updated;
+    }
+
+    public static Meal getNew() {
+        Meal newMeal = new Meal(meal);
+        meal.setDescription("Новая еда");
+        return newMeal;
+    }
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    public static void assertMatch(List<Meal> actual, List<Meal> expected) {
+        assertThat(actual).usingFieldByFieldElementComparator().isEqualTo(expected);
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,127 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.MealTest;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.Assert.assertThrows;
+import static ru.javawebinar.topjava.MealTest.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    static {
+        // Only for postgres driver logging
+        // It uses java.util.logging and logged via jul-to-slf4j bridge
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void get() {
+        Meal meal = service.get(MEAL_USER_ID, USER_ID);
+        assertMatch(meal, userMeal1);
+    }
+
+    @Test
+    public void getNotFound() {
+        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND_MEAL_ID, USER_ID));
+    }
+
+    @Test
+    public void getNotBelong() {
+        assertThrows(NotFoundException.class, () -> service.get(MEAL_USER_ID, ADMIN_ID));
+    }
+
+    @Test
+    public void delete() {
+        service.delete(MEAL_ADMIN_ID, ADMIN_ID);
+        assertThrows(NotFoundException.class, () -> service.get(MEAL_ADMIN_ID, ADMIN_ID));
+    }
+
+    @Test
+    public void deleteNotFound() {
+        assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND_MEAL_ID, USER_ID));
+    }
+
+    @Test
+    public void deleteNotBelong() {
+        assertThrows(NotFoundException.class, () -> service.get(MEAL_ADMIN_ID, USER_ID));
+    }
+
+    @Test
+    public void getAll() {
+        List<Meal> all = service.getAll(USER_ID);
+        assertMatch(all, userMeals);
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+        List<Meal> filteredMeal = service.getBetweenInclusive(LocalDate.of(2020, 1, 30),
+                LocalDate.of(2020, 1, 30), ADMIN_ID);
+        assertMatch(filteredMeal, adminMealsFrom30th);
+    }
+
+    @Test
+    public void getBetweenInclusiveNullParam() {
+        List<Meal> filteredMeal = service.getBetweenInclusive(null, null, ADMIN_ID);
+        assertMatch(filteredMeal, adminMeals);
+    }
+
+    @Test
+    public void update() {
+        Meal updated = MealTest.getUpdated(MEAL_ADMIN_ID);
+        service.update(new Meal(updated), ADMIN_ID);
+        assertMatch(service.get(MEAL_ADMIN_ID, ADMIN_ID), updated);
+    }
+
+    @Test
+    public void updateNotBelong() {
+        Meal updated = MealTest.getUpdated(MEAL_ADMIN_ID);
+        assertThrows(NotFoundException.class, () -> service.update(updated, USER_ID));
+    }
+
+    @Test
+    public void updateNotFound() {
+        Meal updated = MealTest.getUpdated(NOT_FOUND_MEAL_ID);
+        assertThrows(NotFoundException.class, () -> service.update(updated, USER_ID));
+    }
+
+    @Test
+    public void create() {
+        Meal newMeal = MealTest.getNew();
+        Meal created = service.create(new Meal(newMeal), ADMIN_ID);
+        Integer newId = created.getId();
+        newMeal.setId(newId);
+        assertMatch(created, newMeal);
+        assertMatch(service.get(newId, ADMIN_ID), newMeal);
+    }
+
+    @Test
+    public void duplicateDateCreate() {
+        Meal duplicateMeal = new Meal(adminMeal3);
+        duplicateMeal.setId(null);
+        assertThrows(DataAccessException.class, () -> service.create(duplicateMeal, ADMIN_ID));
+    }
+}
